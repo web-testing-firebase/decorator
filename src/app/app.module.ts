c68ccafd9742e298e74de88767cbb4a779823206
jay shah <web.testing.firebase@gmail.com>
2019-07-31 12:40:35 +0530

    decorator initialized

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductAlertsComponent,
+    ProductListComponent,
+    TopBarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
